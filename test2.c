2d5fa9f37f6aca813df69368cf4e0220aa1008f4
Sarah Victoria <sarahvictoria2040@gmail.com>
2025-04-02 23:37:08 -0300

    Meu primeiro commit

---
+++ test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello, World!");
+    printf("Esse é um dos meus \n primeiros tests em C");
+    return 0;
+}
---
+++ testivariaveis.c
@@ -0,0 +1,9 @@
+//variaveis
+//sintaxe type variablename = value;
+#include <stdio.h>
+
+int main () {
+  int myNum = 15;
+  printf("%d", myNum); // Nothing happens
+  return 0;
+}
